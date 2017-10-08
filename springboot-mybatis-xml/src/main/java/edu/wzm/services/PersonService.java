3aeab0ffe008d3d4024c820773051b861cf6eb25
GatsbyNewton <wangzmking@163.com>
2017-10-08 17:25:27 +0800

    serivce layer classes

---
+++ springboot-mybatis-xml/src/main/java/edu/wzm/services/PersonService.java
@@ -0,0 +1,26 @@
+package edu.wzm.services;
+
+import edu.wzm.dao.PersonDao;
+import edu.wzm.entities.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by gatsbynewton on 2017/10/8.
+ */
+@Service
+public class PersonService {
+
+    @Autowired
+    private PersonDao personDao;
+
+    public Person getById(int id){
+        return personDao.getById(id);
+    }
+
+    public List<Person> getAll(){
+        return personDao.getAll();
+    }
+}
