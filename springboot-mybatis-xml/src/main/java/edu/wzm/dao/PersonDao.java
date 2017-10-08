6a04e9c46ef1cbe08125653ba0bfbe4b836376f6
GatsbyNewton <wangzmking@163.com>
2017-10-08 17:24:53 +0800

    Dao layer classes

---
+++ springboot-mybatis-xml/src/main/java/edu/wzm/dao/PersonDao.java
@@ -0,0 +1,21 @@
+package edu.wzm.dao;
+
+import edu.wzm.entities.Person;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * Created by gatsbynewton on 2017/10/8.
+ */
+public interface PersonDao {
+    void insert(@Param("person") Person person);
+
+    Person getById(@Param("id") int id);
+
+    List<Person> getAll();
+
+    void update(@Param("person") Person person);
+
+    void delete(@Param("id") int id);
+}
