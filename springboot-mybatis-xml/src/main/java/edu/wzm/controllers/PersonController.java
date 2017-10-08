6f87372b4e5dafec14dd2c446ea76fa60d4a102f
GatsbyNewton <wangzmking@163.com>
2017-10-08 17:24:19 +0800

    Controllers

---
+++ springboot-mybatis-xml/src/main/java/edu/wzm/controllers/PersonController.java
@@ -0,0 +1,23 @@
+package edu.wzm.controllers;
+
+import edu.wzm.services.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by gatsbynewton on 2017/10/8.
+ */
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+    @Autowired
+    private PersonService personService;
+
+    @RequestMapping("/one")
+    public String getById(@RequestParam("id") int id){
+        return personService.getById(id).toString();
+    }
+}
