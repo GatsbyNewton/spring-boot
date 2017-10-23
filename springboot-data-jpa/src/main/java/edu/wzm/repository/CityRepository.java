06e7b97f08b6b4cb838910bd9ebfe137221e5bd7
GatsbyNewton <wangzmking@163.com>
2017-10-23 23:41:28 +0800

    Spring Data JPA respository class

---
+++ springboot-data-jpa/src/main/java/edu/wzm/repository/CityRepository.java
@@ -0,0 +1,40 @@
+package edu.wzm.repository;
+
+import edu.wzm.entity.City;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * Created by gatsbynewton on 2017/10/15.
+ */
+@Repository
+public interface CityRepository extends JpaRepository<City, Integer>, JpaSpecificationExecutor<City> {
+
+    @Query(value = "select c.id, c.name, c.state, c.country, c.map from city c", nativeQuery = true)
+    List<City> find();
+
+    @Query(value = "SELECT c.id, c.country, c.name, c.state, c.map " +
+            " FROM city c " +
+            " WHERE c.id = :id", nativeQuery = true)
+    City findBy(@Param(value = "id") int id);
+
+    @Modifying
+    @Transactional
+    @Query(value = "update city c set c.name = :name where c.id = :id", nativeQuery = true)
+    int update(@Param("name") String name, @Param("id") int id);
+
+    @Modifying
+    @Transactional
+    @Query(value = "delete from city c where id = :id", nativeQuery = true)
+    int delete(@Param("id") int id);
+
+}
