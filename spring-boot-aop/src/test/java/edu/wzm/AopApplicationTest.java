8837602cf896a123f5f13341827b647a29f32513
GatsbyNewton <wangzmking@163.com>
2018-10-14 23:41:55 +0800

    POM

---
+++ spring-boot-aop/src/test/java/edu/wzm/AopApplicationTest.java
@@ -0,0 +1,20 @@
+package edu.wzm;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple AopApplication.
+ */
+public class AopApplicationTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
