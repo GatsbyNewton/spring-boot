9b24252683c872ecc82f6589dee5bed1ffb1396b
GatsbyNewton <wangzmking@163.com>
2018-10-15 00:09:48 +0800

    修改启动类

--- spring-boot-data-jpa/src/main/java/edu/wzm/DataJpaApplication.java
+++ spring-boot-data-jpa/src/main/java/edu/wzm/DataJpaApplication.java
@@ -1,9 +1,6 @@
 package edu.wzm;
 
-import edu.wzm.repository.CityRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
@@ -18,9 +15,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @EnableJpaRepositories(value = "edu.wzm.repository")
 public class DataJpaApplication {
-
-    @Autowired
-    CityRepository cityRepository;
 
     public static void main( String[] args ) {
         SpringApplication.run(DataJpaApplication.class, args);
